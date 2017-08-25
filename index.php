4bb84067fa8ac5188401ed27069bb2c30ca801ff
Antonio C. Gregorio <antoniogregorio@outlook.com>
2017-08-25 22:01:51 +0000

    resolvido conflito echos

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-<<<<<<< HEAD
     echo "Olá Mundo !!!!";
     echo "Hello world!!!!";
-=======
-    echo "Olá Mundo!!";
-    echo "hello world";
->>>>>>> a63b906a48f729ccc0daa8a429b37db47ff82f33
 ?>
